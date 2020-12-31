a9a774f800014f8b9c0730379ac827755f31c57d
fadhlanhrts <fadhlan.harits@gmail.com>
2020-12-31 17:42:54 +0700

    Initial commit

---
+++ src/Mahasiswa.java
@@ -0,0 +1,12 @@
+
+public class Mahasiswa {
+    String nama;
+    String jurusan;
+    int angkatan;
+
+    public Mahasiswa(String nama, String jurusan, int angkatan){
+        this.nama = nama;
+        this.jurusan = jurusan;
+        this.angkatan = angkatan;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+       Proker prok = new Proker();
+       prok.display();
+    }
+}
+
